c7ecb8aefe08e8a3e7231704792f954cdf4f9346
Chris9224 <kaiyuan9224@gmail.com>
2014-12-25 17:10:25 +0800

    Added JSON result for AJAX

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 	$mysqli = getConnection ();
 	echo $mysqli->host_info;
 	
-	
+	$questions = array();
 	for($difficulty= 1; $difficulty<4; $difficulty++){
 		if ($stmt = $mysqli->prepare("SELECT * FROM table_question WHERE question_difficulty=? ORDER BY RAND() LIMIT 5")) {
 			
 			$stmt->bind_param("s", $difficulty);
-	
+			
 	   		/* execute query */
 	   		$stmt->execute();
 			
@@ -19,26 +19,33 @@
 					
 			/*Fetch results*/
 			while ($stmt->fetch()) {
-			
+				$question = array('question_id' => $question_id,'question_description'=>$question_description);
 		        printf ("<br /> %s %s %s\n", $question_id ,$question_description, $question_difficulty);
 				
 				//Random answers arrangment
 				$numbers = range(1, 4);
 				shuffle($numbers);
 				
+				$answers = array();
 				foreach($numbers as $n){
 					//echo $numbers[i];
 					
 					if($n == 1){
 						echo '<br /> -'.$question_answer1;
+						array_push($answers, $question_answer1);
 					}else if($n == 2){
 						echo '<br /> -'.$question_answer2;
+						array_push($answers, $question_answer2);
 					}else if($n == 3){
 						echo '<br /> -'.$question_answer3;
+						array_push($answers, $question_answer3);
 					}else if($n == 4){
 						echo '<br /> -'.$question_answer4;
+						array_push($answers, $question_answer4);
 					}
 				}
+				$question["answers"] = $answers;
+				array_push($questions, $question);
 		    }
 		}
 		
@@ -47,6 +54,10 @@
 	    /* close statement */
 	    $stmt->close();
 	}
+
+	/*Returns JSON for ajax in future*/
+	echo '<hr /><h1>JSON result</h1>';
+	echo json_encode($questions);
 	$mysqli->close();
 	
 	
