9d8ebf5e1de422c13b0943008e442c023db20531
Jing Tao Tan <jingtao.tan@bertelsmann.de>
2014-12-26 18:07:48 +0800

    Improved question selection logic

--- quiz.php
+++ quiz.php
@@ -1,3 +1,45 @@
+<?php
+require_once 'php/config.php';
+
+$mysqli = getConnection ();
+
+$questions = array();
+for($difficulty = 1; $difficulty <= 3; $difficulty++){
+	if ($stmt = $mysqli->prepare("SELECT * FROM table_question WHERE question_difficulty=? ORDER BY RAND() LIMIT 5")) {
+
+		$stmt->bind_param("s", $difficulty);
+
+		/* execute query */
+		$stmt->execute();
+
+		/* binds result */
+		$stmt->bind_result($question_id, $question_description, $question_difficulty,
+			$question_answer[1], $question_answer[2] ,$question_answer[3] ,$question_answer[4]);
+
+		/*Fetch results*/
+		while ($stmt->fetch()) {
+			$question = array('question_id' => $question_id,'question_description'=>$question_description);
+
+			//Random answers arrangment
+			$numbers = range(1, 4);
+			shuffle($numbers);
+
+			$answers = array();
+			foreach($numbers as $n){
+				// number is the original number of the answer (answer1, answer2, etc)
+				$answer = array('number' => $n, 'text' => $question_answer[$n]);
+				array_push($answers, $answer);
+			}
+			$question["answers"] = $answers;
+			array_push($questions, $question);
+		}
+	}
+	/* close statement */
+	$stmt->close();
+}
+$mysqli->close();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -53,5 +95,8 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="js/bootstrap.min.js"></script>
+	<script>
+		var questions = <?php echo json_encode($questions); ?>
+	</script>
 </body>
 </html>
