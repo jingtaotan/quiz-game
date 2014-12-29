f823c9291ec67aad57b68cc72d9ceb1530e7fe60
Jing Tao Tan <jingtao.tan@bertelsmann.de>
2014-12-29 11:59:32 +0800

    Added basic interactivity for displaying selected questions and progressing through quiz with answer collection and timer

--- quiz.php
+++ quiz.php
@@ -65,28 +65,35 @@ $mysqli->close();
 
 		<div class="header">
 			<div class="timer">
-				<span id="timer-display">01:00</span>
-				<a href="#" id="quit-btn" class="btn btn-danger">Quit Game</a>
+				<span id="timer-txt">01:00.0</span>
+				<a href="#" id="quit-btn" class="btn btn-danger pull-right">Quit Game</a>
 			</div>
 		</div>
 
-		<div class="question">
-			Question will ask you which option is the right one?
+		<div id="quiz-screen">
+			<div class="panel panel-default">
+				<div class="panel-body question" id="question-txt">
+				</div>
+			</div>
+
+			<div class="list-group answers">
+				<a href="#" class="list-group-item answer" id="answer1-txt">
+					&nbsp;
+				</a>
+				<a href="#" class="list-group-item answer" id="answer2-txt">
+					&nbsp;
+				</a>
+				<a href="#" class="list-group-item answer" id="answer3-txt">
+					&nbsp;
+				</a>
+				<a href="#" class="list-group-item answer" id="answer4-txt">
+					&nbsp;
+				</a>
+			</div>
 		</div>
 
-		<div class="answers">
-			<a class="answer">
-				Option 1
-			</a>
-			<a class="answer">
-				Option 2
-			</a>
-			<a class="answer">
-				Option 3
-			</a>
-			<a class="answer">
-				Option 4
-			</a>
+		<div id="finished-screen">
+			<h1>Quiz finished!</h1>
 		</div>
 
 	</div>
@@ -96,7 +103,128 @@ $mysqli->close();
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="js/bootstrap.min.js"></script>
 	<script>
-		var questions = <?php echo json_encode($questions); ?>
+		var questions = <?php echo json_encode($questions); ?>;
+
+		$(document).ready(function() {
+			var answers = [];
+			var questionIndex = 0;
+			var timerInterval;
+			var timeTaken, timeLeft;
+
+			// DOM elements
+			var questionEl = $('#question-txt');
+			var answerEls = $('.answer');
+			var quitBtn = $('#quit-btn');
+			var timerEl = $('#timer-txt');
+
+			var resetControls = function() {
+				answerEls.removeClass('active disabled');
+				answerEls.removeAttr('disabled');
+				answerEls.blur();
+			};
+
+			var disableControls = function() {
+				answerEls.attr('disabled', '');
+				answerEls.addClass('disabled');
+			};
+
+			var putQuestion = function(questionIndex) {
+				// put the question text in
+				var question = questions[questionIndex];
+				questionEl.text(question.question_description);
+				// put the answer texts in
+				// use native for loop for speed, IIFE used to hide the var i
+				(function() {
+					var i;
+					for (i = 0; i < 4; i++) {
+						var answer = question.answers[i];
+						var answerEl = $(answerEls[i]);
+						answerEl.data('number', answer.number);
+						answerEl.text(answer.text);
+					}
+				})();
+			};
+
+			var startQuiz = function() {
+
+				questionEl.text('Ready... set...');
+				disableControls();
+				setTimeout(function() {
+					// set the date we're counting down to
+					var target_date = new Date(new Date().getTime() + 60000); // 1 extra second to show 01:00
+
+					// update the timer display every 1 millisecond
+					timerInterval = setInterval(function () {
+						// find the amount of "seconds" between now and target
+						var current_date = new Date().getTime();
+						var milliseconds_left = (target_date - current_date);
+
+						if ( milliseconds_left <= 0 ) {
+							milliseconds_left = 0;
+							finishQuiz();
+						}
+
+						timeLeft = new Date(milliseconds_left);
+						timeTaken = new Date(60000 - milliseconds_left);
+
+						var minutes = parseInt(milliseconds_left / 60000);
+						if ( minutes < 10 ) {
+							minutes = "0" + minutes.toString();
+						}
+						var seconds = parseInt((milliseconds_left % 60000) / 1000);
+						if ( seconds < 10 ) {
+							seconds = "0" + seconds.toString();
+						}
+						var tenths_second = parseInt((milliseconds_left % 1000) / 100); // only display 1 decimal place after seconds
+						timerEl.text(minutes + ":" + seconds + "." + tenths_second);
+					}, 1);
+
+					putQuestion(questionIndex++);
+					resetControls();
+				}, 1000);
+
+			};
+
+			var finishQuiz = function() {
+				clearInterval(timerInterval);
+				console.log('Quiz finished.');
+				console.log('Time taken: ' + timeTaken);
+				console.log('Time left: ' + timeLeft);
+
+				$('#quiz-screen').hide();
+				$('#finished-screen').show();
+			};
+
+			quitBtn.on('click', function() {
+				finishQuiz();
+			});
+
+			answerEls.on('mousedown', function(event) {
+				var el = $(this);
+				if ( !el.attr('disabled') ) {
+					answerEls.removeClass('active');
+					$(this).addClass('active');
+				}
+			});
+			answerEls.on('click', function(event) {
+				event.preventDefault
+				var answerEl = $(this);
+				if ( !answerEl.attr('disabled') ) {
+					var answerNum = answerEl.data('number')
+					answers.push(answerNum);
+					console.log('answerNum: ' + answerNum);
+					if ( questionIndex == questions.length ) {
+						finishQuiz();
+					} else {
+						putQuestion(questionIndex++);
+						resetControls();
+					}
+				}
+			});
+
+			// start with the first question
+			startQuiz();
+		});
 	</script>
 </body>
 </html>
