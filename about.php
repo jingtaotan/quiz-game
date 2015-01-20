0567294fe4569765d29708cd33915e110e5aaba3
Chris <kaiyuan9224@gmail.com>
2015-01-20 15:12:01 +0800

    update on about page

--- about.php
+++ about.php
@@ -25,157 +25,153 @@ require_once 'php/config.php';
 			<?php getNavBar("about"); ?>
 			<div class="container-fluid">
 				<div id="content" class="row">
-					<div class="col-md-12 thumbnail text-center">
-						<h2>Who we are?</h2>
-						<p>Arvato is a market-leading solution provider for consumer-
-						-oriented industries.
-						As a global services provider arvato supports business
-						customers all over the world and has successfully shaped their
-						customer relationships. More than 66,000 employees in
-						almost 40 countries design and implement customized
-						solutions for integrated services. The group is a wholly owned
-						subsidiary of Bertelsmann.
-						</p>
-						<p>Arvato is comprised of seven solution groups:</p>
-						<ul class="list-group">
-                            <li class="list-group-item"><span class="label label-info">CRM (Customer Relationship Management) Solutions</span></li>
-                            <li class="list-group-item"><span class="label label-info">Digital Marketing</span></li>
-                            <li class="list-group-item"><span class="label label-info">Financial Solutions</span></li>
-                            <li class="list-group-item"><span class="label label-info">IT Solutions</span></li>
-                            <li class="list-group-item"><span class="label label-info">Print Solutions</span></li>
-                            <li class="list-group-item"><span class="label label-info">Replication</span></li>
-                            <li class="list-group-item"><span class="label label-info">SCM (Supply Chain Management) Solutions</span></li>
-                        </ul>
-                    </div>
+					<div class="col-md-12 thumbnail">
+						<h2 class="parrentHidden"><span class="glyphicon glyphicon-minus-sign childHidden" aria-hidden="true"></span><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Who we are?</h2>
+						<div class="childHidden text-center">
+							<p>
+								Arvato is a market-leading solution provider for consumer-
+								-oriented industries.
+								As a global services provider arvato supports business
+								customers all over the world and has successfully shaped their
+								customer relationships. More than 66,000 employees in
+								almost 40 countries design and implement customized
+								solutions for integrated services. The group is a wholly owned
+								subsidiary of Bertelsmann.
+							</p>
+							<p>
+								Arvato is comprised of seven solution groups:
+							</p>
+							<ul class="list-group">
+								<li class="list-group-item">
+									<span class="label label-info">CRM (Customer Relationship Management) Solutions</span>
+								</li>
+								<li class="list-group-item">
+									<span class="label label-info">Digital Marketing</span>
+								</li>
+								<li class="list-group-item">
+									<span class="label label-info">Financial Solutions</span>
+								</li>
+								<li class="list-group-item">
+									<span class="label label-info">IT Solutions</span>
+								</li>
+								<li class="list-group-item">
+									<span class="label label-info">Print Solutions</span>
+								</li>
+								<li class="list-group-item">
+									<span class="label label-info">Replication</span>
+								</li>
+								<li class="list-group-item">
+									<span class="label label-info">SCM (Supply Chain Management) Solutions</span>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 				<div id="content" class="row">
-					<div class="col-md-12 thumbnail text-center">
-						<h2>What do we do?</h2>
-						<p>Our solutions include data management, customer service,
-						customer management services, supply chain management,
-						digital distribution, financial services and customized IT
-						services, as well as the full range of services related to the
-						production and distribution of printed materials and digital
-						storage media.
-						</p>
-						<p>
-						Our business is centered on consumer-driven industries such
-						as telecommunications, IT/Internet, energy, automotive and
-						healthcare. Our strategic objective is to create sustainable
-						growth through systematic innovations, internationalization,
-						flexibility, customer focus and partnerships.
-						arvato IT Solutions uses the technology, talent, and expertise
-						of nearly 3,000 people at more than 25 sites throughout the
-						world to create integrated, future-proof business infrastructures
-						that help our clients become more agile and competitive,
-						and enable them to deliver new standards of service to their
-						customers. We create streamlined digital processes that
-						support innovative business models while providing operation
-						and support services. Being a part of the arvato network and
-						belonging to Bertelsmann, we have the unique capability to
-						create entire value chains.
-						</p>
-						<p>
-						arvato IT Solutions offer an exceptional combination of
-						international IT engineering excellence, the open mindset of a
-						global player, and the dedication of employees. We also ensure
-						that all our customer relationships are as personally rewarding
-						and long-lasting as they are successful. The multitude of
-						successful projects completed with leading companies around
-						the world shows how true partnership leads to results, reliably
-						and efficiently.
-						<p>
-						In Malaysia, we develop the software and systems which form
-						the technology component of arvato’s solutions. As we are
-						expanding in Malaysia and South East Asia, we are constantly
-						looking for new blood to join us and build their IT career with
-						us. Talk to us and create your own career with arvato
-						Bertelsmann.
-						</p>
-						<p>
-						We focus on implementing projects for leading businesses in
-						the most diverse industries with extended expertise in retail,
-						travel and transport, media, manufacturing, public sector and
-						utilities. For services as BPM, Cloud Computing, CRM and
-						eCommerce we provide profound competence.
-						We are specialized in the following technologies:
-						</p>
-						<ul class="list-group">
-						    <li class="list-group-item"><span class="label label-info">Java Technologies</span></li>
-						    <li class="list-group-item"><span class="label label-info">Microsoft Technologies</span></li>
-						    <li class="list-group-item"><span class="label label-info">SAP Business Solutions</span></li>
-						</ul>
-					</div>
-				</div>
-				<script>
-					window.fbAsyncInit = function() {
-						FB.init({
-							appId : '1575617865987702',
-							xfbml : true,
-							version : 'v2.2'
-						});
-					}; ( function(d, s, id) {
-							var js,
-							    fjs = d.getElementsByTagName(s)[0];
-							if (d.getElementById(id)) {
-								return;
-							}
-							js = d.createElement(s);
-							js.id = id;
-							js.src = "//connect.facebook.net/en_US/sdk.js";
-							fjs.parentNode.insertBefore(js, fjs);
-						}(document, 'script', 'facebook-jssdk'));
-				</script>
-				<div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
-				<hr />
-				<div id="footer" class="row">
-					<div class="col-md-4">
-						<ul>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-						</ul>
-					</div>
-					<div class="col-md-4">
-						<ul>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-						</ul>
-					</div>
-					<div class="col-md-4">
-						<ul>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-							<li>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</li>
-						</ul>
+					<div class="col-md-12 thumbnail">
+						<h2 class="parrentHidden"><span class="glyphicon glyphicon-minus-sign childHidden" aria-hidden="true"></span>
+						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> What do we do?</h2>
+						<div class="childHidden text-center">
+							<p>
+								Our solutions include data management, customer service,
+								customer management services, supply chain management,
+								digital distribution, financial services and customized IT
+								services, as well as the full range of services related to the
+								production and distribution of printed materials and digital
+								storage media.
+							</p>
+							<p>
+								Our business is centered on consumer-driven industries such
+								as telecommunications, IT/Internet, energy, automotive and
+								healthcare. Our strategic objective is to create sustainable
+								growth through systematic innovations, internationalization,
+								flexibility, customer focus and partnerships.
+								arvato IT Solutions uses the technology, talent, and expertise
+								of nearly 3,000 people at more than 25 sites throughout the
+								world to create integrated, future-proof business infrastructures
+								that help our clients become more agile and competitive,
+								and enable them to deliver new standards of service to their
+								customers. We create streamlined digital processes that
+								support innovative business models while providing operation
+								and support services. Being a part of the arvato network and
+								belonging to Bertelsmann, we have the unique capability to
+								create entire value chains.
+							</p>
+							<p>
+								arvato IT Solutions offer an exceptional combination of
+								international IT engineering excellence, the open mindset of a
+								global player, and the dedication of employees. We also ensure
+								that all our customer relationships are as personally rewarding
+								and long-lasting as they are successful. The multitude of
+								successful projects completed with leading companies around
+								the world shows how true partnership leads to results, reliably
+								and efficiently.
+							<p>
+								In Malaysia, we develop the software and systems which form
+								the technology component of arvato’s solutions. As we are
+								expanding in Malaysia and South East Asia, we are constantly
+								looking for new blood to join us and build their IT career with
+								us. Talk to us and create your own career with arvato
+								Bertelsmann.
+							</p>
+							<p>
+								We focus on implementing projects for leading businesses in
+								the most diverse industries with extended expertise in retail,
+								travel and transport, media, manufacturing, public sector and
+								utilities. For services as BPM, Cloud Computing, CRM and
+								eCommerce we provide profound competence.
+								We are specialized in the following technologies:
+							</p>
+							<ul class="list-group">
+								<li class="list-group-item">
+									<span class="label label-info">Java Technologies</span>
+								</li>
+								<li class="list-group-item">
+									<span class="label label-info">Microsoft Technologies</span>
+								</li>
+								<li class="list-group-item">
+									<span class="label label-info">SAP Business Solutions</span>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
+			<script>
+				window.fbAsyncInit = function() {
+					FB.init({
+						appId : '1575617865987702',
+						xfbml : true,
+						version : 'v2.2'
+					});
+				};
+				( function(d, s, id) {
+						var js,
+						    fjs = d.getElementsByTagName(s)[0];
+						if (d.getElementById(id)) {
+							return;
+						}
+						js = d.createElement(s);
+						js.id = id;
+						js.src = "//connect.facebook.net/en_US/sdk.js";
+						fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));
+			</script>
+			<div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
+			<hr />
+			
+		</div>
 		</div>
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="js/bootstrap.min.js"></script>
+		<script>
+			$(".parrentHidden").click(function() {
+				$(this).children('.glyphicon').toggle();
+				$(this).next('.childHidden').slideToggle();
+			});
+		</script>
 	</body>
 </html>
