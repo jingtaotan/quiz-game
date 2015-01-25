b27b2bd717e7f90704b18addd93e20f944c951e8
Chris <kaiyuan9224@gmail.com>
2015-01-25 22:46:50 +0800

    modify for mandatory question with difficulty(4). So far only two mandatory question for the db

--- quiz.php
+++ quiz.php
@@ -9,7 +9,7 @@ $_SESSION['submitted'] = null;
 session_commit();
 
 $questions = array();
-for ($difficulty = 1; $difficulty <= 3; $difficulty++) {
+for ($difficulty = 1; $difficulty <= 4; $difficulty++) {
 	if ($stmt = $mysqli -> prepare("SELECT * FROM table_question WHERE question_difficulty=? ORDER BY RAND() LIMIT ".LIMIT)) {
 
 		$stmt -> bind_param("s", $difficulty);
